4fbe4648583669e3b482b426246c3b9e03c249f9
souro89 <souro89@gmail.com>
2018-03-10 13:31:58 +0400

    JPA Spring Data Inititalization

---
+++ src/main/java/com/datakettle/spring5demo/Bootstrap/DevBootstrap.java
@@ -0,0 +1,50 @@
+package com.datakettle.spring5demo.Bootstrap;
+
+import com.datakettle.spring5demo.model.Author;
+import com.datakettle.spring5demo.model.Book;
+import com.datakettle.spring5demo.repositories.AuthorRepository;
+import com.datakettle.spring5demo.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        Author eric = new Author("Eric","Evans");
+        Book ddd = new Book("Domain Driven Design","1234","Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        Author rod = new Author("Rod","Johnson");
+        Book noEJB = new Book("J2EE Development without EJB","23444","Worx");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+
+    }
+
+}
